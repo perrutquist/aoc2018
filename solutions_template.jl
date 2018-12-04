50a08f6b7ed4cb083d021e4b2ada6e24d1eb6b73
Per Rutquist <per.rutquist@gmail.com>
2018-12-04 09:11:39 +0100

    template for writing solutions

---
+++ solutions_template.jl
@@ -0,0 +1,35 @@
+# For each day, write a `scan` and a `solve` method.
+# (Or skip the `scan` method and let `solve` start from a list of strings.)
+
+# --------------------------------------------------------------------
+day = 1
+
+function scan(::Val{day}, s)
+
+end
+
+function solve(::Val{day}, part, v)
+    if part == Val(1)
+
+    else
+
+    end
+end
+
+# --------------------------------------------------------------------
+day = 2
+
+function scan(::Val{day}, s)
+
+end
+
+function solve(::Val{day}, part, v)
+    if part == Val(1)
+
+    else
+
+    end
+end
+
+# --------------------------------------------------------------------
+# and so on...
