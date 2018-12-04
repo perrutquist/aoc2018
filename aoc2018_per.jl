e41fe7f513f482caae9e3705f2a079dceaec9103
Per Rutquist <per.rutquist@gmail.com>
2018-12-04 07:16:42 +0100

    day 4

--- aoc2018_per.jl
+++ aoc2018_per.jl
@@ -11,7 +11,7 @@ module AoC
 using InteractiveUtils
 
 "Scan one line of input for a given day"
-scan(day::Integer, s) = scanline(val(day), s)
+scan(day::Integer, s) = scan(Val(Int(day)), s)
 
 "Real all the input lines for a given day"
 function getdata(day::Val{D}) where {D}
@@ -19,14 +19,14 @@ function getdata(day::Val{D}) where {D}
     isfile(file) || return nothing
     map(s->scan(day, s), eachline(file))
 end
-getdata(day::Integer) = getdata(Val(day))
+getdata(day::Integer) = getdata(Val(Int(day)))
 
 "Solve the problem for a given day and part, given a vector of scanned lines."
-solve(day, part, v) = solve(Val(day), Val(part), v)
+solve(day::Integer, part::Integer, v) = solve(Val(Int(day)), Val(Int(part)), v)
 
-solve(::Val, ::Val, v) = nothing # fallback
+solve(::Val, p, v) = nothing # fallback
 
-# For each day we need a `scan` and two `solves` methods.
+# For each day we need a `scan` and a `solve` method.
 # day 0 can be copy-pasted as a starting point
 
 # --------------------------------------------------------------------
@@ -35,10 +35,12 @@ day = 0
 function scan(::Val{day}, s)
 end
 
-function solve(::Val{day}, ::Val{1}, v)
-end
+function solve(::Val{day}, p, v)
+    if p == Val(1)
 
-function solve(::Val{day}, ::Val{2}, v)
+    else
+
+    end
 end
 
 # --------------------------------------------------------------------
@@ -84,25 +86,59 @@ function scan(::Val{day}, s)
     parse.((Int, Int, Int, Int, Int), Tuple(m.captures))
 end
 
-function day3overlaps(v)
+function solve(::Val{day}, p, v)
     mx = reduce((a,b) -> max.(a,b), v)
     M = zeros(Int, mx[2]+mx[4], mx[3]+mx[5])
     for c in v
         M[c[2]+1:c[2]+c[4], c[3]+1:c[3]+c[5]] .+= 1
     end
-    M
-end
-
-function solve(::Val{day}, ::Val{1}, v)
-    sum(day3overlaps(v) .> 1)
-end
-
-function solve(::Val{day}, ::Val{2}, v)
-    M = day3overlaps(v)
-    for c in v
-        all(M[c[2]+1:c[2]+c[4], c[3]+1:c[3]+c[5]] .== 1) && return c[1]
+    if p === Val(1)
+        return sum(day3overlaps(v) .> 1)
+    else
+        for c in v
+            all(M[c[2]+1:c[2]+c[4], c[3]+1:c[3]+c[5]] .== 1) && return c[1]
+        end
     end
 end
+
+# --------------------------------------------------------------------
+day = 4
+
+const BIG = 10000 # Assumed max number of guards
+
+function scan(::Val{day}, s)
+    m = match(r"\[(\d*)-(\d*)-(\d*) (\d*):(\d*)\] (.*)", s)
+    s = parse.((Int, Int, Int, Int, Int), Tuple(m.captures[1:5]))
+    g = match(r"Guard #(\d*) begins shift", m.captures[6])
+    id = g === nothing ? 0 : parse(Int, g.captures[1])
+    (366*s[1] + 31*s[2] + s[3]+(s[4]==23), s[5]-60*(s[4]==23), id, m.captures[6]=="falls asleep")
+end
+
+function solve(::Val{day}, p, v)
+    v = sort(v, by=i->10000*i[1]+i[2])
+    id = 0
+    t = 0
+    s = zeros(Int, BIG, 60)
+    for (xx, m, i, sl) in v
+        if i > 0
+            id = i
+        elseif sl
+            t = m
+        else # wakes up
+            s[id, t+1:m] .+= 1
+        end
+    end
+    if p == Val(1)
+        tot = sum(s, dims=2)
+        (tx, ix) = findmax(vec(tot))
+        (xx, mn) = findmax(s[ix,:])
+        return (ix*(mn-1))
+    else
+        (tx, ix) = findmax(s)
+        return ix[1]*(ix[2]-1)
+    end
+end
+
 
 # --------------------------------------------------------------------
 # Loop over puzzles, load data, compute and print results
