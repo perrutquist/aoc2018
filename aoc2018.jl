34e36129cf8fe6caf468ea3285530414a93854a6
Per Rutquist <per.rutquist@gmail.com>
2018-12-08 14:01:30 +0100

    measure run time twice per puzzle

--- aoc2018.jl
+++ aoc2018.jl
@@ -29,7 +29,7 @@ const timing = zeros(25, 2)
 
 Loop over puzzles, load data, compute and print results
 """
-function solveall(;reverse=false, clipboard=false, setglobals=false)
+function solveall(;reverse=false, clipboard=false, setglobals=false, twice=false)
     t0 = time_ns()
     N = 0
     D = 0
@@ -37,38 +37,48 @@ function solveall(;reverse=false, clipboard=false, setglobals=false)
     for day in order(1:25)
         ld = getlines(day)
         if ld !== nothing
+            lines[day] = ld
             D += 1
             if setglobals
                 Dn = Symbol("D$day")
-                dd = scan.(day, ld)
+                dd = scan.(day, deepcopy(ld))
                 println("\nDay $day: ", eltype(dd))
                 Main.eval(:( $Dn = $dd ))
             end
             for part in order(1:2)
                 td0 = time_ns()
-                r = solve(day, part, ld)
+                r = solve(day, part, deepcopy(ld))
                 global solution[day, part] = r
-                t = 1e-9*(time_ns() - td0)
-                global timing[day, part] = t
+                t = time_ns() - td0 # nanoseconds
+                global timing[day, part] = t*1e-9 # seconds
                 if r !== nothing
                     N += 1
                     print("Day $day, part $part: ")
                     if N == 1 && clipboard
                         printstyled(r, bold=true)
                         InteractiveUtils.clipboard(string(r))
-                        print(" (copied to clipboard)")
+                        print("  (copied to clipboard)")
                     else
                        print(r)
                     end
-                    Printf.@printf " ( %.6f seconds )\n" t
+                    Printf.@printf "  Time: %.3f ms" t*1e-6
+
+                    if twice
+                        td1 = time_ns()
+                        r2 = solve(day, part, deepcopy(ld))
+                        t2 = time_ns() - td1
+                        @assert r == r2
+                        Printf.@printf ", 2:nd time: %.3f ms." t2*1e-6
+                    end
+
+                    println()
                 end
             end
         end
     end
     if D != 0
         Printf.@printf("\nSolved %d problems in %.6f seconds.\n", N, sum(timing))
-        println("Timings may include JIT-compilation time (but not reading data from disk).")
-        println("Run AoC.solveall() a second time to get timings without JIT-compilation.")
+        println("First timings may include JIT-compilation time, 2:nd timings measure only execution.")
         #Printf.@printf("Total time, including file reads: %.2f seconds.\n", 1e-9*(time_ns()-t0))
     else
         println("No data files found!")
@@ -104,6 +114,6 @@ end
 # Solve all puzzles.
 # Reverse order, since we're usually interested in the most recent result.
 # Copy to clipboard to make it easier to submit to contest.
-solveall(reverse=true, clipboard=true, setglobals=true)
+solveall(reverse=true, clipboard=true, setglobals=true, twice=true)
 
 end # module
