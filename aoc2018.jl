543092a6912d33991187b5b39f0d617195e29253
Per Rutquist <per.rutquist@gmail.com>
2018-12-12 15:28:56 +0100

    new macro: @main

--- aoc2018.jl
+++ aoc2018.jl
@@ -6,6 +6,37 @@ using BenchmarkTools
 
 include("solutions_per.jl") # Change this line to use your own code instead
 
+"""
+    main(a::Symbol, v)
+
+Assign `v` to a variable in the `Main` module, with name given by the symbol `a`,
+and display it to stdout.
+
+This can be useful for debugging, but should probably never be used in
+production code.
+"""
+function main(a::Symbol, v, verbose)
+    Main.eval( :( $a = $v ) )
+    if verbose
+        println(a, " =")
+        display(v)
+    end
+end
+
+"""
+    @main x
+
+Copy a variable (e.g. `x`) into the `Main` module, and display it to stdout.
+
+This can be useful for debugging, but should probably never be used in
+production code.
+"""
+macro main(a::Symbol, verbose::Bool=true)
+    quote
+        main($(QuoteNode(a)), $(esc(a)), $verbose)
+    end
+end
+
 "Parse one line of input for a given day and convert to a more useful form"
 scan(day::Integer, s) = scan(Val(Int(day)), s)
 scan(::Val, s) = s # fallback
